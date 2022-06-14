17ec12b327f61f677498453f9e3979c26a975991
GentleSevenV <183672570@qq.com>
2022-06-14 16:59:12 +0800

    集成axios，并且创建axios-demo.ts进行测试

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/service/axios-demo'
 
 // 全局注册Element-plus，全局注册完之后还要在下面.use(ElementPlus)
 // import ElementPlus from 'element-plus'
