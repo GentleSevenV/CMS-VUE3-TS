9139e6651ede49dbd228b6fd9e30c17d13a0bbbf
GentleSevenV <183672570@qq.com>
2022-06-14 15:31:52 +0800

    集成Element-plus，并配置按需引入组件

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 全局注册Element-plus，全局注册完之后还要在下面.use(ElementPlus)
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
 createApp(App).use(store).use(router).mount('#app')
