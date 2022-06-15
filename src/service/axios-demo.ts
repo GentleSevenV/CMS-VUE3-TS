cfc1268d4a48d436a61b3d9d3e61e5f0f281eaef
GentleSevenV <183672570@qq.com>
2022-06-15 17:49:21 +0800

    添加了axios.request和axios.all的使用方法

--- src/service/axios-demo.ts
+++ src/service/axios-demo.ts
@@ -25,3 +25,24 @@ axios
   .then((res) => {
     console.log(res.data)
   })
+
+axios
+  .request({
+    url: 'http://123.207.32.32:8000/home/multidata',
+    method: 'get'
+    // ...
+  })
+  .then((res) => {
+    console.log(res.data)
+  })
+
+axios
+  .all([
+    axios.get('http://httpbin.org/get', { params: { name: 'Lilei', age: 18 } }),
+    axios.post('http://httpbin.org/post', { data: { name: 'Hanmeimei', age: 19 } })
+  ])
+  .then((res) => {
+    console.log(res) //因为当前一次性发送了2个请求，打印出res是一个数组
+    console.log(res[0].data) //从res数组中取出第1个请求结果
+    console.log(res[1].data) //从res数组中取出第2个请求结果
+  })
