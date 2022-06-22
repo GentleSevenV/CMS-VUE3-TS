ebf6ccc6f9775e24a61347528398726eb0778925
GentleSevenV <183672570@qq.com>
2022-06-22 17:46:34 +0800

    添加vuex，并且调用setUpStore将本地缓存中的token/userInfo/userMenus添加进vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,25 @@
 import { createStore } from 'vuex'
+import { IRootState } from './type'
+import login from './login/login'
 
-export default createStore({
-  state: {},
+const store = createStore<IRootState>({
+  state: () => {
+    return {
+      name: 'Lilei',
+      age: 18
+    }
+  },
   getters: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    login
+  }
 })
+
+// 通过在main.ts中调用setUpStore此函数，则通过store.dispatch去执行子模块（login模块）action中的loadLocalLogin，去将本地缓存中的token/userInfo/userMenus添加进vuex中
+export function setUpStore() {
+  store.dispatch('login/loadLocalLogin')
+}
+
+export default store
