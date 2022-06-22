edd0c02c0f155e1e0b2ea39cb93512999d2d56a9
GentleSevenV <183672570@qq.com>
2022-06-22 17:17:41 +0800

    添加登录后刷新页面，行setUpStore这个函数，重新添加登录信息到vuex中；另外注释了一些测试内容

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { setUpStore } from './store'
 
 import 'normalize.css'
 import '@/assets/css/index.less'
-import lrRequest from '@/service/index'
+// import lrRequest from '@/service/index'
 
 // 测试查看axios demo案例时打开
 // import '@/service/axios-demo'
@@ -29,28 +30,33 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
+// 由于vuex中的数据是存储在内存中的，所以为了防止登录之后刷新页面，vuex中的数据被清空，
+// 我们需要通过执行setUpStore这个函数，重新将本地中的token/userInfo/userMenus添加进vuex中
+// 因为刷新页面，程序会重新找到main.ts这个项目入口文件并执行，所以将此方法在这里调用是最合适不过了
+setUpStore()
+
 // 使用环境变量
 // console.log(BASE_URL, TIME_OUT)
 
 // 请求中没有自己的拦截器：
-interface IDataType {
-  data: any
-  returnCode: string
-  success: boolean
-}
+// interface IDataType {
+//   data: any
+//   returnCode: string
+//   success: boolean
+// }
 
-lrRequest
-  .request<IDataType>({
-    url: '/home/multidata',
-    method: 'GET',
-    responseType: 'json',
-    showLoading: true
-  })
-  .then((res) => {
-    console.log(res.data)
-    console.log(res.returnCode)
-    console.log(res.success)
-  })
+// lrRequest
+//   .request<IDataType>({
+//     url: '/home/multidata',
+//     method: 'GET',
+//     responseType: 'json',
+//     showLoading: true
+//   })
+//   .then((res) => {
+//     console.log(res.data)
+//     console.log(res.returnCode)
+//     console.log(res.success)
+//   })
 
 // 请求中有自己的拦截器：
 // lrRequest.request({
